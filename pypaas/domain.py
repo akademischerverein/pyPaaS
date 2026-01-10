97a7953223a9475d7c4efaf0dc6b42d97fd85f23
Lea <autoit4you@users.noreply.github.com>
2026-01-11 00:39:16 +0100

    update tls config

--- pypaas/domain.py
+++ pypaas/domain.py
@@ -184,7 +184,7 @@ class Domain(object):
                 'ssl_config',
                 options.main.get(
                     'ssl_config',
-                    # https://ssl-config.mozilla.org/#server=nginx&version=1.14.2&config=intermediate&openssl=1.1.1d&guideline=5.6
+                    # https://ssl-config.mozilla.org/#server=nginx&version=1.22.1&config=intermediate&openssl=3.0.18&guideline=5.6
                     '''
                         ssl_dhparam /etc/ssl/private/httpd/dhparam.pem;
                         ssl_session_timeout 1d;
@@ -192,6 +192,7 @@ class Domain(object):
                         ssl_session_tickets off;
                         ssl_protocols TLSv1.2 TLSv1.3;
                         ssl_ciphers 'ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384';
+                        ssl_ecdh_curve X25519:prime256v1:secp384r1;
                         ssl_prefer_server_ciphers off;
                         add_header Strict-Transport-Security "max-age=63072000" always;
                     '''
